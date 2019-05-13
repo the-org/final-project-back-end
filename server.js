f014c1b2369a52b421b9fffa7eaad86fb73cb349
Levi Porter <levi@levibrooke.com>
2019-05-13 16:33:31 -0700

    Modify server to have a listen callback

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-app.listen(process.env.PORT||3000);
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
