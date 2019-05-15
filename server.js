feeabbbbedcca9982dbc1cc671df5ea0a128bd32
Matt Burger <matt.burger1@outlook.com>
2019-05-15 15:40:51 -0700

    route DELETE /saved/:userID/:articleID functional

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const allMedia = require('./routes/all-media.js');
 const article = require('./routes/article.js');
 const save = require('./routes/save.js');
 const saved = require('./routes/saved.js');
+const deleteEntry = require('./routes/delete.js');
 
 const PORT = process.env.PORT || 3000;
 
@@ -20,6 +21,7 @@ app.use(allMedia);
 app.use(article);
 app.use(save);
 app.use(saved);
+app.use(deleteEntry);
 
 app.get('/', (req, res) => {
   res.send('Hello World');
