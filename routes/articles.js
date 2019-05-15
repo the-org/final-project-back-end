93883f0c5460ac447ffa56b8b8491acc422ad106
Matt Burger <matt.burger1@outlook.com>
2019-05-14 17:36:45 -0700

    started dev of articles route: end of day stopping point

---
+++ routes/articles.js
@@ -0,0 +1,27 @@
+//route for /article
+
+'user strict';
+
+const express = require('express');
+const { Client } = require('pg');
+const router = express.Router;
+
+//connection to psql
+const dbUrl = 'postgres://localhost:5432/devhubdb';
+const client = new Client({
+  connectionString: dbUrl,
+});
+client.connect(err => {
+  if (err) {
+    console.error(err);
+  }
+  console.log('Connected to pg');
+});
+
+//route
+router.route('/:id')
+  .get(req, res) => {
+    //grab request parameter for article id
+    let articleId = '';
+
+  }
